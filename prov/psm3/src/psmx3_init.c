nmorey/libfabric

commit 3ef633408edfc636d28b0302ce2ab3e66b2f6dca

prov/psm3: update provider to sync with IEFS 11.5.1.0.3
Updates:
- Improved Direct GPU access for Intel GPUs

Signed-off-by: Breyer, Scott <scott.breyer@intel.com>
Signed-off-by: Scott Breyer <scott.breyer@intel.com>

prov/psm3/src/psmx3_init.c
@@ -713,6 +713,18 @@ static int psmx3_getinfo(uint32_t api_version, const char *node,
 
 	PSMX3_INFO(&psmx3_prov, FI_LOG_CORE,"\n");
 
+	__builtin_cpu_init();
+	if (!__builtin_cpu_supports(PSM3_MARCH)) {
+		PSMX3_INFO(&psmx3_prov, FI_LOG_CORE,
+			"CPU does not support '%s'.\n", PSM3_MARCH);
+		OFI_INFO_STR(&psmx3_prov,
+			(__builtin_cpu_supports("avx2") ? "AVX2" :
+				(__builtin_cpu_supports("avx") ? "AVX" :
+					(__builtin_cpu_supports("sse4.2") ? "SSE4.2" : "unknown"))),
+			PSM3_MARCH, "CPU Supports", "PSM3 Built With");
+		goto err_out;
+	}
+
 	if (psmx3_init_prov_info(hints, &prov_info))
 		goto err_out;
 

prov/psm3/README
@@ -1,11 +1,28 @@
 This is derived from libfabric source.  See libfabric for README, AUTHORS, COPYING
 and other notices.
 
+Before building, set CONFIG_OPTS
+   If building for GPU (Intel GPU or NVIDIA):
+      CONFIG_OPTS="--with-oneapi-ze=/usr"
+      -or-
+      CONFIG_OPTS="--with-cuda=/usr/local/cuda"
+   else (CPU only - CONFIG_OPTS should be empty)
+      CONFIG_OPTS=
 
 To Build PSM3 OFI Provider:
-1. ./configure
+1. ./configure $CONFIG_OPTS
 2. make -j
 
 To Build PSM3 OFI Provider RPM:
-1. ./configure
-2. make rpm
+1. ./configure $CONFIG_OPTS
+2. export VER=$(rpmspec --query --srpm --queryformat "%{version}\n" libpsm3-fi.spec)
+3. mkdir -p rpmbuild/{SPECS,SOURCES}
+4. git archive --format=tar --prefix=libpsm3-fi-${VER}/ HEAD | bzip2 > rpmbuild/SOURCES/libpsm3-fi-${VER}.tar.bz2
+5. rpmbuild --define "_topdir $(pwd)/rpmbuild" --define "configopts LDFLAGS=-Wl,--build-id ${CONFIG_OPTS}" -ba libpsm3-fi.spec
+
+To Build PSM3 OFI Provider deb:
+1. ./configure $CONFIG_OPTS
+2. export VER=$(grep Version: libpsm3-fi.spec | cut -d' ' -f2)
+3. git archive --format=tar --prefix=libpsm3-fi-${VER}/ HEAD | tar xf -
+4. cd libpsm3-fi-${VER}
+5. dpkg-buildpackage

prov/psm3/VERSION
@@ -1,1 +1,1 @@
-3_5_0_0
+3_5_1_0

prov/psm3/configure.ac
@@ -570,14 +570,11 @@ AS_IF([test ! -z "$PSM_CUDA"], [with_cuda=/usr/local/cuda])
 have_cuda=0
 AS_IF([test x"$with_cuda" != x"no"],
       [
-        FI_CHECK_PACKAGE([cuda],
-                 [cuda_runtime.h],
-                 [cudart],
-                 [cudaMemcpy],
-                 [],
-                 [$with_cuda],
-                 [],
-                 [have_cuda=1])
+        _FI_CHECK_PACKAGE_HEADER([cuda],
+                [cuda_runtime.h],
+                [$with_cuda],
+                [have_cuda=1],
+                [AC_MSG_ERROR([CUDA headers not found. Cannot enable CUDA DL])])
       ])
 
 AS_IF([test x"$with_cuda" != x"no" && test -n "$with_cuda" && test "$have_cuda" = "0" ],
@@ -601,14 +598,11 @@ AC_ARG_WITH([oneapi-ze],
 have_oneapi_ze=0
 AS_IF([test x"$with_oneapi_ze" != x"no"],
       [
-        FI_CHECK_PACKAGE([ze],
+        _FI_CHECK_PACKAGE_HEADER([ze],
                 [level_zero/ze_api.h],
-                [ze_loader],
-                [zeInit],
-                [],
                 [$with_oneapi_ze],
-                [],
-                [have_oneapi_ze=1])
+                [have_oneapi_ze=1],
+                [AC_MSG_ERROR([OneAPI ZE headers not found. Cannot enable ZE DL])])
       ])
 
 have_drm=0
@@ -849,9 +843,12 @@ AS_IF([test ! -z "$CC" && ( test "x${CC%% *}" = "xicc" || test "x${CC%% *}" = "x
       [ dnl ICC/ICX
         CFLAGS="$CFLAGS -Werror -xATOM_SSE4.2 -DPSM_AVX512 -fpic -fPIC -D_GNU_SOURCE -DPACK_STRUCT_STL=packed,"
         LDFLAGS="$LDFLAGS -Wc,-static-intel"
+        PSM3_MARCH="avx2"
       ], [ dnl GCC/other
         CFLAGS="$CFLAGS -Werror -mavx2 -fpic -fPIC -funwind-tables -Wformat -Wformat-security"
+        PSM3_MARCH="avx2"
       ])
+AC_DEFINE_UNQUOTED([PSM3_MARCH], ["$PSM3_MARCH"], [PSM3 built with instruction set])
 AS_IF([test ! -z "$PSM_CPPFLAGS"], [CPPFLAGS="$CPPFLAGS $PSM_CPPFLAGS"], [])
 AS_IF([test ! -z "$PSM_CFLAGS"], [CFLAGS="$CFLAGS $PSM_CFLAGS"], [])
 
@@ -944,6 +941,7 @@ AC_SUBST(PSM3_PROV_VER_MAJOR)
 AC_SUBST(PSM3_PROV_VER_MINOR)
 AC_SUBST(PSM3_PROV_VER_MAINT)
 AC_SUBST(PSM3_PROV_VER_PATCH)
+AC_SUBST(PSM3_MARCH)
 dnl Set during Make.
 dnl AC_SUBST(PSM3_BUILD_TIMESTAMP, ["<Unknown>"])
 dnl AC_SUBST(PSM3_SRC_CHECKSUM, ["<Unknown>"])

prov/psm3/configure.m4
@@ -20,6 +20,7 @@ AC_DEFUN([FI_PSM3_CONFIGURE],[
 
      PSM3_HAL_INST=""
      PSM3_HAL_CNT=0
+     PSM3_MARCH=""
 
      psm3_happy=1
      AS_IF([test x"$enable_psm3" != x"no"],
@@ -120,6 +121,7 @@ AC_DEFUN([FI_PSM3_CONFIGURE],[
             ],[
                 AC_MSG_RESULT([yes])
                 PSM3_ARCH_CFLAGS="-msse4.2"
+                PSM3_MARCH="sse4.2"
             ],[
                 psm3_happy=0
                 AC_MSG_RESULT([no])
@@ -141,6 +143,7 @@ AC_DEFUN([FI_PSM3_CONFIGURE],[
             ],[
                 AC_MSG_RESULT([yes])
                 PSM3_ARCH_CFLAGS="-mavx"
+                PSM3_MARCH="avx"
             ],[
                 psm3_happy=0
                 AC_MSG_RESULT([no])
@@ -162,6 +165,7 @@ AC_DEFUN([FI_PSM3_CONFIGURE],[
             ],[
                 AC_MSG_RESULT([yes])
                 PSM3_ARCH_CFLAGS="-mavx2"
+                PSM3_MARCH="avx2"
             ],[
                 AC_MSG_RESULT([no])
             ])
@@ -387,6 +391,8 @@ AC_DEFUN([FI_PSM3_CONFIGURE],[
      AC_SUBST(psm3_LIBS)
      AC_SUBST(PSM3_HAL_CNT)
      AC_SUBST(PSM3_HAL_INST)
+     AC_DEFINE_UNQUOTED([PSM3_MARCH], ["$PSM3_MARCH"], [PSM3 built with instruction set])
+     AC_SUBST(PSM3_MARCH)
 
      PSM3_IEFS_VERSION=m4_normalize(m4_esyscmd([cat prov/psm3/VERSION]))
      AC_SUBST(PSM3_IEFS_VERSION)

prov/psm3/psm3/hal_sockets/sockets_hal.c
@@ -358,4 +358,7 @@ static void __attribute__ ((constructor)) __psmi_hal_sockets_constructor(void)
 {
 	psm3_hal_register_instance((psmi_hal_instance_t*)&psm3_sockets_hi);
 }
+/* Dummy var used to force Static compilation to include HAL objects when linking */
+int sockets_hal_called = -1;
+
 #endif /* PSM_SOCKETS */

prov/psm3/psm3/hal_verbs/verbs_hal.c
@@ -174,9 +174,13 @@ static void psm3_hfp_verbs_mq_init_defaults(struct psm2_mq *mq)
 		mq->hfi_thresh_rv = (~(uint32_t)0); // disable rendezvous
 	}
 	mq->hfi_thresh_tiny = PSM_MQ_NIC_MAX_TINY;
-#if defined(PSM_CUDA) || defined(PSM_ONEAPI)
+#ifdef PSM_CUDA
 	if (PSMI_IS_GPU_ENABLED)
 		mq->hfi_base_window_rv = 2097152;
+#endif
+#ifdef PSM_ONEAPI
+	if (PSMI_IS_GPU_ENABLED)
+		mq->hfi_base_window_rv = 512*1024;
 #endif
 	// we parse mr_cache_mode and rv_gpu_cache_size here so we can cache it
 	// once per EP open, even if multi-rail or multi-QP
@@ -346,4 +350,7 @@ static void __attribute__ ((constructor)) __psmi_hal_verbs_constructor(void)
 {
 	psm3_hal_register_instance((psmi_hal_instance_t*)&psm3_verbs_hi);
 }
+/* Dummy var used to force Static compilation to include HAL objects when linking */
+int verbs_hal_called = -1;
+
 #endif /* PSM_VERBS */

prov/psm3/psm3/include/utils_debug.h
@@ -57,6 +57,7 @@
 #define UTILS_DEBUG_H
 
 #include <stdio.h>
+#include <time.h>
 
 // See below for macros and comments about these settings
 
@@ -141,6 +142,30 @@ int psm3_get_myrank_count();	// -1 if unknown
 int psm3_get_mylocalrank();	// -1 if unknown
 int psm3_get_mylocalrank_count();	// -1 if unknown
 
+#ifdef PSM_TS_USER
+#define PSM3_TIME_FMT "[%s.%09lu] "
+#define PSM3_TIME_ARG \
+	tbuf, ts.tv_nsec
+#define PSM3_GETTIME \
+	struct timespec ts; \
+	struct tm tm; \
+	char tbuf[40]; \
+		      \
+	clock_gettime(CLOCK_REALTIME, &ts); \
+	tzset(); \
+	localtime_r(&ts.tv_sec, &tm); \
+	strftime(tbuf, 40, "%F %T", &tm);
+#else
+#define PSM3_TIME_FMT "[%lu.%09lu] "
+#define PSM3_TIME_ARG \
+	ts.tv_sec, ts.tv_nsec
+#define PSM3_GETTIME \
+	struct timespec ts;	\
+				\
+	clock_gettime(CLOCK_REALTIME, &ts);
+
+#endif
+
 #if _HFI_DEBUGGING
 
 extern char psm3_mylabel[];
@@ -154,23 +179,27 @@ extern void psm3_dump_gpu_buf(uint8_t *buf, uint32_t len);
 #define _HFI_UNIT_ERROR(unit, fmt, ...) \
 	do { \
 		_Pragma_unlikely \
-		printf("%s.%s: " fmt, psm3_mylabel, __progname, \
+		PSM3_GETTIME \
+		printf(PSM3_TIME_FMT "%s.%s: " fmt, PSM3_TIME_ARG, psm3_mylabel, __progname, \
 		       ##__VA_ARGS__); \
 	} while (0)
 
 #define _HFI_ERROR(fmt, ...) \
 	do { \
 		_Pragma_unlikely \
-		printf("%s.%s: " fmt, psm3_mylabel, __progname, \
+		PSM3_GETTIME \
+		printf(PSM3_TIME_FMT "%s.%s: " fmt, PSM3_TIME_ARG, psm3_mylabel, __progname, \
 		       ##__VA_ARGS__); \
 	} while (0)
 
 #define _HFI_INFO(fmt, ...) \
 	do { \
 		_Pragma_unlikely \
-		if (unlikely(psm3_dbgmask&__HFI_INFO))  \
-			printf("%s.%s: " fmt, psm3_mylabel, __func__, \
+		if (unlikely(psm3_dbgmask&__HFI_INFO)) {  \
+			PSM3_GETTIME \
+			printf(PSM3_TIME_FMT "%s.%s: " fmt, PSM3_TIME_ARG, psm3_mylabel, __func__, \
 			       ##__VA_ARGS__); \
+		} \
 	} while (0)
 
 #define __HFI_PKTDBG_ON unlikely(psm3_dbgmask & __HFI_PKTDBG)
@@ -178,17 +207,21 @@ extern void psm3_dump_gpu_buf(uint8_t *buf, uint32_t len);
 #define __HFI_DBG_WHICH(which, fmt, ...) \
 	do { \
 		_Pragma_unlikely \
-		if (unlikely(psm3_dbgmask&(which))) \
-			fprintf(psm3_dbgout, "%s.%s: " fmt, psm3_mylabel, __func__, \
-			       ##__VA_ARGS__); \
+		if (unlikely(psm3_dbgmask&(which))) {\
+			PSM3_GETTIME \
+			fprintf(psm3_dbgout, PSM3_TIME_FMT "%s.%s: " fmt, PSM3_TIME_ARG, psm3_mylabel, \
+				__func__, ##__VA_ARGS__); \
+		} \
 	} while (0)
 
 #define __HFI_DBG_WHICH_NOFUNC(which, fmt, ...) \
 	do { \
 		_Pragma_unlikely \
-		if (unlikely(psm3_dbgmask&(which))) \
-			fprintf(psm3_dbgout, "%s: " fmt, psm3_mylabel, \
+		if (unlikely(psm3_dbgmask&(which))) { \
+			PSM3_GETTIME \
+			fprintf(psm3_dbgout, PSM3_TIME_FMT "%s: " fmt, PSM3_TIME_ARG, psm3_mylabel, \
 			       ##__VA_ARGS__); \
+		} \
 	} while (0)
 
 #define _HFI_DBG(fmt, ...) __HFI_DBG_WHICH(__HFI_DBG, fmt, ##__VA_ARGS__)
@@ -214,7 +247,8 @@ extern void psm3_dump_gpu_buf(uint8_t *buf, uint32_t len);
 #define _HFI_DBG_ALWAYS(fmt, ...) \
 	do { \
 		_Pragma_unlikely \
-		fprintf(psm3_dbgout, "%s.%s: " fmt, psm3_mylabel, __func__, \
+		PSM3_GETTIME \
+		fprintf(psm3_dbgout, PSM3_TIME_FMT "%s.%s: " fmt, PSM3_TIME_ARG, psm3_mylabel, __func__, \
 			       ##__VA_ARGS__); \
 	} while (0)
 
@@ -245,12 +279,14 @@ extern void psm3_dump_gpu_buf(uint8_t *buf, uint32_t len);
 
 #define _HFI_UNIT_ERROR(unit, fmt, ...) \
 	do { \
-		printf("%s: " fmt, "", ##__VA_ARGS__); \
+		PSM3_GETTIME \
+		printf(PSM3_TIME_FMT "%s: " fmt, PSM3_TIME_ARG, "", ##__VA_ARGS__); \
 	} while (0)
 
 #define _HFI_ERROR(fmt, ...) \
 	do { \
-		printf("%s: " fmt, "", ##__VA_ARGS__); \
+		PSM3_GETTIME \
+		printf(PSM3_TIME_FMT "%s: " fmt, PSM3_TIME_ARG, "", ##__VA_ARGS__); \
 	} while (0)
 
 #define _HFI_INFO(fmt, ...)

commit fd049a0a053502a85b839a715fc6f9fdbfc4439a

psm3: fix SIGILL on system not supporting AVX
Even though code was added to not use the PSM3 provider on system
without AVX/AVX2 support, logs triggered by the detection code can
be using AVX instructions causing psmx3_getinfo to SIGILL

Move the detection even earlier in the provider init and use OFI
standard print functions to avoid using any code that might have
been compiled with AVX instructions.

Fixes: 3ef633408edf ("prov/psm3: update provider to sync with IEFS 11.5.1.0.3")
Signed-off-by: Nicolas Morey <nmorey@suse.com>

prov/psm3/src/psmx3_init.c
@@ -680,18 +680,6 @@ static int psmx3_getinfo(uint32_t api_version, const char *node,
 
 	PSMX3_INFO(&psmx3_prov, FI_LOG_CORE,"\n");
 
-	__builtin_cpu_init();
-	if (!__builtin_cpu_supports(PSM3_MARCH)) {
-		PSMX3_INFO(&psmx3_prov, FI_LOG_CORE,
-			"CPU does not support '%s'.\n", PSM3_MARCH);
-		OFI_INFO_STR(&psmx3_prov,
-			(__builtin_cpu_supports("avx2") ? "AVX2" :
-				(__builtin_cpu_supports("avx") ? "AVX" :
-					(__builtin_cpu_supports("sse4.2") ? "SSE4.2" : "unknown"))),
-			PSM3_MARCH, "CPU Supports", "PSM3 Built With");
-		goto err_out;
-	}
-
 	if (psmx3_init_prov_info(hints, &prov_info))
 		goto err_out;
 
@@ -895,6 +883,19 @@ struct fi_provider psmx3_prov = {
 
 PROVIDER_INI
 {
+	__builtin_cpu_init();
+	if (!__builtin_cpu_supports(PSM3_MARCH)) {
+		FI_INFO(&core_prov, FI_LOG_CORE,
+			"PSM3: CPU does not support '%s'.\n", PSM3_MARCH);
+		OFI_INFO_STR(&core_prov,
+			(__builtin_cpu_supports("avx2") ? "AVX2" :
+				(__builtin_cpu_supports("avx") ? "AVX" :
+					(__builtin_cpu_supports("sse4.2") ? "SSE4.2" : "unknown"))),
+			PSM3_MARCH, "CPU Supports", "PSM3 Built With");
+		return NULL;
+	}
+
+
 	psmx3_prov.version = get_psm3_provider_version();
 
 	PSMX3_INFO(&psmx3_prov, FI_LOG_CORE, "build options: VERSION=%u.%u=%u.%u.%u.%u, "